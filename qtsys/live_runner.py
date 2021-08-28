f0dfb8ed07d880f01d16e743fc3466dde443e913
Ethan Tong <ethantong.studio@gmail.com>
2021-08-27 23:56:53 -0500

    add scheduling

--- qtsys/live_runner.py
+++ qtsys/live_runner.py
@@ -36,37 +36,4 @@ def run(
   market_data=YahooData(),
 
 ):
-  print('bootstrap schedule')
-  schedule = _create_schedule()
-  selection_date = None
-  while True:
-    today = pd.Timestamp(_now().date()) 
-    if today > schedule.index[-1]:
-      print('updating schedule')
-      schedule = _create_schedule()
-    if today not in schedule.index:
-      print(_now(), 'not a trading day. sleep for an hour...')
-      time.sleep(3600)
-    else:
-      #SElECTING
-      if not selection_date or selection_date + selection_interval <= datetime.now().date():
-        while _now().hour < 7:
-          print(_now(), 'waiting for selection time. sleep for a minute')
-          time.sleep(60)
-        print(_now(), 'selecting assets')
-        selection_date = _now().date()
-        # selection = universe_selector.on_selection(selection_date.strftime('%Y-m-%d'))
-
-      # TRADING
-      today = _now().strftime('%Y-%m-%d')
-      next_trading_time = schedule.loc[today]['market_open'].astimezone(ny_tz) + offset
-      closing_time = schedule.loc[today]['market_close'].astimezone(ny_tz)
-      if _now() > next_trading_time:
-        next_trading_time += math.ceil((_now() - next_trading_time) / interval) * interval
-      print('next trading time', next_trading_time)
-      while next_trading_time < closing_time:
-        if _now() >= next_trading_time:
-          print(_now(), 'trading in action')
-          next_trading_time += interval
-          time.sleep(interval.total_seconds() - 60)
-        time.sleep(1)
+  pass
