f748d9bfe517cdcab50e4358fe2da07229945f85
Ethan Tong <ethantong.studio@gmail.com>
2021-08-29 18:16:38 -0500

    try arctic

--- qtsys/data/tradier_data.py
+++ qtsys/data/tradier_data.py
@@ -1,33 +1,27 @@
 import asyncio
-from qtsys.data.util import resample_bar_data
-
-from qtsys.client.tradier import TradierClient
 import pandas as pd
+
+from qtsys.data.util import resample_bar_data
+from qtsys.client.tradier import TradierClient
 from qtsys.data.market_data import MarketData
+from qtsys.client import arctic_lib
 
 
 class TradierData(MarketData):
-  interval_param = {
-    '1d': 'daily',
-    '1m': '1min',
-    '5m': '5min',
-    '15m': '15min',
-    '30m': '15min',
-    '1h': '15min'
-  }
 
-  resample_param = {
-    '30m': '30min',
-    '1h': '60min'
+  _resample_interval = {
+    '2min': '1min',
+    '30min': '15min',
+    '60min': '15min',
   }
 
   def __init__(self):
     self.client = TradierClient(trading_mode=False, account_type='live')
 
-  def download_bars(self, symbols: str, start=None, end=None, interval='1d'):
+  def download_bars(self, symbols: str, start=None, end=None, interval='60min'):
     params_list = [self._create_data_params(symbol, start, end, interval) for symbol in symbols.split(' ')]
     loop = asyncio.get_event_loop()
-    if interval[-1] == 'd':
+    if interval == 'daily':
       results = loop.run_until_complete(self.client.async_get_all(loop, '/v1/markets/history', params_list))
       bars_dict = dict(results)
       for symbol, bars in bars_dict.items():
@@ -43,17 +37,23 @@ class TradierData(MarketData):
         df = pd.json_normalize(bars['series']['data'])
         df.set_index('time', inplace=True)
         df.index = pd.to_datetime(df.index) # type: ignore
-        if interval in self.resample_param.keys():
-          df = resample_bar_data(df, self.resample_param[interval])
+        if interval not in self._api_interval:
+          df = resample_bar_data(df, interval)
         bars_dict[symbol] = df
       return bars_dict
    
-  def save_bars(self, symbols: str, start=None, end=None, interval='1d'):
-    self._historical_bars = self.download_bars(symbols, start, end, interval)
+  def save_bars(self, symbols: str, start=None, end=None, interval='60min'):
+    bars_dict = self.download_bars(symbols, start, end, interval)
+    for symbol, df in bars_dict.items():
+      arctic_lib.write('tradier', interval, symbol, df)
+
   
   def get_historical_bars(self, symbol, current_date):
-    return self._historical_bars[symbol][:current_date][:-1]
+    # return self._historical_bars[symbol][:current_date][:-1]
+    pass
 
   def _create_data_params(self, symbol, start, end, interval):
-    return {'symbol': symbol, 'interval': self.interval_param[interval], 'start': start, 'end': end}
+    return {'symbol': symbol, 'interval': interval, 'start': start, 'end': end}
+
+  def _create 
 
