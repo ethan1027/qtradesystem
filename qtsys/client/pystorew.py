4f9597b273a2b2815ebb6adcf250731d348e4183
Ethan Tong <ethantong.studio@gmail.com>
2021-09-05 15:35:21 -0500

    write pystore

--- qtsys/client/pystorew.py
+++ qtsys/client/pystorew.py
@@ -5,18 +5,29 @@ store = ps.store('mystore')
 
 def write_bars(provider, interval, symbol, df):
   c = store.collection(f'{provider}.{interval}')
-  c.append(symbol, df)
+  _write_or_append(c, symbol, df)
+
 
 def read_bars(provider, interval, symbol):
   c = store.collection(f'{provider}.{interval}')
-  return c.item(symbol)
+  return c.item(symbol).data
 
-def write_selection(broker_id: str, df):
-  c = store.collection(f'{broker_id}')
-  c.append('selection', df)
+def write_selection(broker_id, df):
+  c = store.collection(broker_id)
+  _write_or_append(c, 'selection', df)
+
+def read_selection(broker_id, dt):
+  c = store.collection(broker_id)
+  return c.item('selection').data[dt]
 
 def write_order(broker_id, df):
   pass
 
 def write_positions(broker_id, df):
   pass
+
+def _write_or_append(collection: ps.collection, item, df):
+  if item not in collection.list_items():
+    collection.write(item, df)
+  else:
+    collection.append(item, df)
